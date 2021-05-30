cbc10aab795ab342c244ff84ecc04b9913431f89
abbasrahmanii <absrn26@gmail.com>
2021-05-30 15:02:50 +0430

    2.AboutSection

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,11 @@
+import React from "react";
+//Import Pages
+import AboutSection from "../components/AboutSection"
+
+const AboutUs = ()=> {
+    return(
+        <AboutSection />
+    )
+}
+
+export default AboutUs;
