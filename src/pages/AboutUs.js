951d5d9cf78fd8db828ba1217c8f22736c49422e
abbasrahmanii <absrn26@gmail.com>
2021-05-30 16:34:34 +0430

    4. Faq Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,12 +2,14 @@ import React from "react";
 //Import Pages
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
+import FagSection from "../components/FagSection";
 
 const AboutUs = () => {
     return(
         <>
             <AboutSection />
             <ServicesSection />
+            <FagSection />
         </>
     );
 };
