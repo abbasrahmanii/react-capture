535143eb764bddb3c3108edfa26259f31cec8c28
abbasrahmanii <absrn26@gmail.com>
2021-06-01 17:29:21 +0430

    12. Framer Motion

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,25 +1,37 @@
 import React from 'react';
 import home1 from '../img/home1.png';
-//Styled
 import {About,Description,Hide,Image} from "../styles";
+//Framer Motin
+import { motion } from "framer-motion";
 
 const AboutSection = () => {
+    const container = {
+        hidden: {x: 100},
+        show: { x: 0, transition: { duration: 0.75, ease: "easeOut", staggerChildren: 0.5 } } 
+    };
+    const titleAnim = {
+        hidden: {opacity: 0},
+        show: { opacity: 1, transition: { duration: 2 } } 
+    };
+
     return(
         <About>
             <Description>
-                <div className="title">
+                <motion.div variants={container} initial="hidden" animate="show" className="title">
                     <Hide>
-                        <h2>We work to make</h2>
+                        <motion.h2 variants={titleAnim} >
+                            We work to make
+                        </motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>your <span>dreams</span> come</h2>
+                        <motion.h2 variants={titleAnim}>your <span>dreams</span></motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>true.</h2>
+                        <motion.h2 variants={titleAnim}>come true.</motion.h2>
                     </Hide>
                     <p>Contact us for any photography or videography ideas that you have. whe have professionals with amazing skills.</p>
                     <button>Contact Us</button>
-                </div>
+                </motion.div>
             </Description>
             <Image>
                     <img src={home1} alt="photographer" />
