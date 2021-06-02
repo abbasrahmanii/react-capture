8222d8cf57576cea4edf0cb9f2f718f3618de1f1
abbasrahmanii <absrn26@gmail.com>
2021-06-02 19:55:21 +0430

    14. About Animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,6 +3,7 @@ import home1 from '../img/home1.png';
 import {About,Description,Hide,Image} from "../styles";
 //Framer Motin
 import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../components/animation";
 
 const AboutSection = () => {
     return(
@@ -10,28 +11,28 @@ const AboutSection = () => {
             <Description>
                 <motion.div>
                     <Hide>
-                        <motion.h2>
+                        <motion.h2 variants={titleAnim}>
                             We work to make
                         </motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2>your <span>dreams</span></motion.h2>
+                        <motion.h2 variants={titleAnim}>your <span>dreams</span></motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2>come true.</motion.h2>
+                        <motion.h2 variants={titleAnim}>come true.</motion.h2>
                     </Hide>
-                    <p>Contact us for any photography or videography ideas that you have. whe have professionals with amazing skills.</p>
-                    <button>Contact Us</button>
+                    <motion.p variants={fade}>
+                        Contact us for any photography or videography ideas that you have. whe have professionals with amazing skills.
+                    </motion.p>
+                    <motion.button variants={fade}>Contact Us</motion.button>
                 </motion.div>
             </Description>
             <Image>
-                    <img src={home1} alt="photographer" />
+                    <motion.img variants={photoAnim} src={home1} alt="photographer" />
             </Image>
         </About>
     )
 };
-
-//Styled Components
 
 
 export default AboutSection;
--- src/components/animation.js
+++ src/components/animation.js
@@ -1,6 +1,6 @@
 export const pageAnimation = {
     hidden: {
-        opacity: 0
+        opacity: 0,
         y: 300,
     },
     show: {
@@ -8,13 +8,56 @@ export const pageAnimation = {
         y: 0,
         transition: {
             duration: 1,
+            when: "beforeChildren",
+            staggerChildren: 0.25,
         }
     },
     exit: {
         opacity: 0,
         y: 300,
         transition: {
-            duration: 1,
+            duration: 0.75,
+            ease: "easeOut",
         }
     }
 };
+
+export const titleAnim = {
+    hidden: {
+        y: 200
+    },
+    show: {
+        y:0,
+        transition: {
+            duration: 1
+        }
+    }
+};
+
+export const fade = {
+    hidden: {
+        opacity: 0,
+    },
+    show: {
+        opacity: 1,
+        transition: {
+            duration: 0.75,
+            ease: "easeOut",
+        }
+    }
+};
+
+export const photoAnim = {
+    hidden: {
+        opacity: 0,
+        scale: 1.5,
+    },
+    show: {
+        opacity: 1,
+        scale: 1,
+        transition: {
+            duration: 0.75,
+            ease: "easeOut",
+        }
+    }
+}
